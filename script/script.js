aaac6a8a2a6b0c0c6cd9e15aca800367732c0336
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2019-12-13 16:32:39 +0530

    Header mostly done, Grid-ing the body.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-/*All Scripting for the Website*/
+$("document").ready(function(){
+  //Navbar Toggling of Hide/Show Classes
+  $(".menu").click(function() {
+    $(".menu").toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
