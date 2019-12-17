ab7c6f4cd6266c5ae11d2468d1cc025866c6d0e4
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2019-12-17 15:11:17 +0530

    Need to Fix Activites + Star Rating Search

--- script/script.js
+++ script/script.js
@@ -5,22 +5,39 @@ $("document").ready(function(){
     $(".navbar-menu").toggleClass("active");
   });
 
-  
-  // Writing Destinations to Search Bar
-  var jsonDestinations = [];
+  //Write to an Array without duplicating Elements
+  function PushToListWithoutDuplicate(array, item) {
+    if (array.includes(item) === false) {
+      array.push(item);
+    }
+  }
+
+  // Writing to Search Bar
+  var jsonDestinations = []; // Destinations Array
+  var jsonActivities = []; // Activities Array
   if ($("#search-destinations-select").length) {
     $.getJSON('../json/locations.json', function(data) {
-      var outputSelect= "<label for='search-destinations'>Destinations</label><br>";
-      outputSelect+= "<select name='search-destinations' id='search-destinations'>";
-      outputSelect+= "<option value='none'>--None Selected--</option>";
+      var activitiesDisplay = ""; //Activities Display
+      var destinationDisplay = "<label for='search-destinations'>Destinations</label><br>"; //Destinations Display
+      destinationDisplay += "<select name='search-destinations' id='search-destinations'>";
+      destinationDisplay += "<option value='none'>--None Selected--</option>";
+      //Destinations
       for (var i in data.resorts) {
         if (jsonDestinations.includes(data.resorts[i].destination) === false) {
-          outputSelect+="<option value='"+data.resorts[i].destination+"'>"+data.resorts[i].destination+"</option>";
+          destinationDisplay+="<option value='"+data.resorts[i].destination+"'>"+data.resorts[i].destination+"</option>";
           jsonDestinations.push(data.resorts[i].destination);
         }
+        //Activities
+        for (var x in data.resorts[i].activities) {
+          if (jsonActivities.includes(data.resorts[i].activities[x]) === false) {
+            activitiesDisplay += "<label><input type='checkbox' name='searchActivities' value='"+data.resorts[i].activities[x]+"'>"+data.resorts[i].activities[x]+"</label><br>";
+            jsonActivities.push(data.resorts[i].activities[x]);
+          }
+        }
       }
-      outputSelect += "</select>";
-      document.getElementById("search-destinations-select").innerHTML=outputSelect;
+      destinationDisplay += "</select>"; // Close Destination Display
+      document.getElementById("search-destinations-select").innerHTML=destinationDisplay; // Destination Printing
+      document.getElementById("search-activities-box").innerHTML+=activitiesDisplay; // Activities Printing
     });
   }
 
@@ -33,31 +50,58 @@ $("document").ready(function(){
   function searchFunction(masterForm) {
     var searchParameters = ["none","none",[],"none"]; //Destination, Comfort Levels
     var searchVerifiedLocations = []; //Verified by algorithm
-    for (var i in jsonDestinations) {
-      if (masterForm["search-destinations"].value == "none") {
-        searchParameters[0]= "none";
-      }
-      else if (masterForm["search-destinations"].value === jsonDestinations[i]) {
-        searchParameters[0] = masterForm["search-destinations"].value;
-        searchParameters[2].push(masterForm["search-destinations"].value);
-        searchParameters[2].push(masterForm["search-destinations"].value);
-        searchParameters[2].push(masterForm["search-destinations"].value);
+    var searchedLocationsDisplay = ""; // Locations Write to Page
+
+    for (var m in masterForm.searchActivities) {
+      if (masterForm.searchActivities[m].checked) {
+        searchParameters[2].push(masterForm.searchActivities[m].value); //Set Chosen Activities
       }
     }
-    
+
     searchParameters[0] = masterForm["search-destinations"].value; //Set Destination
     searchParameters[1] = masterForm["search-comfort-level"].value; // Set Comfort Level
-    alert(searchParameters);
-    
 
     $.getJSON('../json/locations.json', function(data) {
+      // Checking Algorithm and add to searchVerifiedLocations
       for (var i in data.resorts) {
-        if (searchParameters[0] == data.resorts[i].destination) {
-          searchVerifiedLocations.push(data.resorts[i].id);
+        if ((searchParameters[0] == data.resorts[i].destination) && (searchParameters[1] === data.resorts[i].comfortLevel)) {
+          for (var n in searchParameters[2]) {
+            if (data.resorts[i].activities.indexOf(searchParameters[2][n]) > -1 == true) {
+              PushToListWithoutDuplicate(searchVerifiedLocations, i);
+              break;
+            }
+          }
+          PushToListWithoutDuplicate(searchVerifiedLocations, i);
+          continue;
+        }
+        else if ((searchParameters[0] == "none") || (searchParameters[1] == "none") || searchParameters[2] == null) {
+          for (var n in searchParameters[2]) {
+            if (data.resorts[i].activities.indexOf(searchParameters[2][n]) > -1 == true) {
+              PushToListWithoutDuplicate(searchVerifiedLocations, i);
+            }
+          }
+          if (searchParameters[0] == data.resorts[i].destination) {
+            PushToListWithoutDuplicate(searchVerifiedLocations, i);
+          }
+          else if (searchParameters[1] == data.resorts[i].comfortLevel) {
+            PushToListWithoutDuplicate(searchVerifiedLocations, i);
+          }
         }
       }
-      document.getElementById("search-results").innerHTML += searchVerifiedLocations;
+      console.log(searchVerifiedLocations);
+      
+      // Printing to Page
+      for (var y in searchVerifiedLocations) {
+        searchedLocationsDisplay += "<div style='border: 1px solid black; padding: 1rem;'>";
+        searchedLocationsDisplay += "<p> Resort ID: "+data.resorts[searchVerifiedLocations[y]].id+"</p>";
+        searchedLocationsDisplay += "<p> Destination: "+data.resorts[searchVerifiedLocations[y]].name+"</p>";
+        searchedLocationsDisplay += "<p> Destination: "+data.resorts[searchVerifiedLocations[y]].destination+"</p>";
+        searchedLocationsDisplay += "<p> Price: "+data.resorts[searchVerifiedLocations[y]].price+"</p>";
+        searchedLocationsDisplay += "<p> Comfort Level: "+data.resorts[searchVerifiedLocations[y]].comfortLevel+"</p>";
+        searchedLocationsDisplay += "<p> Activities Offered: "+data.resorts[searchVerifiedLocations[y]].activities+"</p>";
+        searchedLocationsDisplay += "</div>";
+      }
+      document.getElementById("search-results").innerHTML = searchedLocationsDisplay;
     });
-    
   }
 });
