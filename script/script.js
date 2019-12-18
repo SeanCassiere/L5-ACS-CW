b5a1dff452334f7ac4de7c43643a6d2bd7a0e028
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2019-12-18 11:05:21 +0530

    Date Validation Created, need to apply to ALL

--- script/script.js
+++ script/script.js
@@ -6,20 +6,14 @@ $("document").ready(function(){
   });
 
   $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 300,
-      max: 3000,
-      values: [ 500, 2500 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
-    });
-  });
+    $('#search-comfort-level').selectmenu(); // Comfort Level Select Menu
+    $('#search-destinations-btn').button(); // Search Button
+    // http://jsfiddle.net/gaby/WArtA/
+    $("#search-date-to").datepicker({ dateFormat: 'yy-mm-dd' }); // Search To Date
+    $("#search-date-from").datepicker({ dateFormat: 'yy-mm-dd' }).bind("change", function(){ var minValue = $(this).val(); minValue = $.datepicker.parseDate("yy-mm-dd", minValue); minValue.setDate(minValue.getDate()+1); $("#search-date-to").datepicker( "option", "minDate", minValue ); }); // Search From Date
+    
+    $( "#slider-range" ).slider({ range: true, min: 300, max: 3000, values: [ 500, 2500 ], slide: function( event, ui ) { $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] ); } });
 
-  $( function() {
-    $('#search-comfort-level').selectmenu();
-    $('#search-destinations-btn').button();
   });
 
 
@@ -59,11 +53,12 @@ $("document").ready(function(){
     });
   }
 
-  // Search Button Launch Searcher //
+  // Search Button Launch Searcher
   $("#search-destinations-btn").click(function() {
     searchFunction(document.forms["search-form"]);
   });
 
+  //Function to compare array1 against array2 to find if ALL the elements of array1 exist in array2
   function arrayItemComparer(arr, arrDb){
     var checkStone = 0;
     for (var i in arr) {
@@ -77,33 +72,52 @@ $("document").ready(function(){
   function searchFunction(masterForm) {
     var searchPriceRange = $('#slider-range').slider("values");
     console.log(searchPriceRange);
-    var searchParameters = ["none","none",[],"none"]; //Destination, Comfort Levels, Activities
+    var searchParameters = ["none","none",[],"none","none"]; //Destination, Comfort Levels, Activities
     var searchVerifiedLocations = []; //Verified by algorithm
     var searchedLocationsDisplay = "<h3>Results:</h3>"; // Locations Write to Page
 
+    //Adding the User selected Activities into SearchParameters[2]
     for (var m in masterForm.searchActivities) {
       if (masterForm.searchActivities[m].checked) {
         searchParameters[2].push(masterForm.searchActivities[m].value); //Set Chosen Activities
       }
     }
-
+    //Adding to custom Search Parameters
     searchParameters[0] = masterForm["search-destinations"].value; //Set Destination
     searchParameters[1] = masterForm["search-comfort-level"].value; // Set Comfort Level
+    searchParameters[3] = masterForm["search-date-from"].value; // Set From Date
+    searchParameters[4] = masterForm["search-date-to"].value; // Set To Date
+    console.log(searchParameters[3], searchParameters[4]);
 
     $.getJSON('/json/locations.json', function(data) {
       // Checking Algorithm and add to searchVerifiedLocations
+      function dateValidationAndSearchResult() {
+        if ( (Date.parse(searchParameters[3]) >= Date.parse(data.resorts[i].startDate)) && (Date.parse(searchParameters[4]) <= Date.parse(data.resorts[i].endDate)) ) {
+          console.log("Date Start + End Passed");
+          PushToListWithoutDuplicate(searchVerifiedLocations, i); return true;
+        } else if ( (Date.parse(searchParameters[3]) >= Date.parse(data.resorts[i].startDate)) && (Date.parse(searchParameters[3]) <= Date.parse(data.resorts[i].endDate)) ) {
+          console.log("Date Start Passed");
+          PushToListWithoutDuplicate(searchVerifiedLocations, i); return true;
+        } else if ( (Date.parse(data.resorts[i].startDate) <= Date.parse(searchParameters[4])) && (Date.parse(data.resorts[i].endDate) >= Date.parse(searchParameters[4])) ) {
+          console.log("Date End Passed");
+          PushToListWithoutDuplicate(searchVerifiedLocations, i); return true;
+        } else if ((searchParameters[3] == "") && (searchParameters[4] == "")) {
+          console.log('No Dates Entered');
+          PushToListWithoutDuplicate(searchVerifiedLocations, i); return true;
+        }
+      }
       for (var i in data.resorts) {
         if ((searchParameters[0] != "none") && (searchParameters[1] != "none") && (searchParameters[2].length != 0)) {
           console.log('Destination + Comfort + Activities');
           if ((arrayItemComparer(searchParameters[2], data.resorts[i].activities) == true) && (searchParameters[2].length !== 0) && (searchParameters[0] == data.resorts[i].destination) && (searchParameters[1] === data.resorts[i].comfortLevel) && (data.resorts[i].price > searchPriceRange[0]) && (data.resorts[i].price < searchPriceRange[1])) {
             console.log('Entered Destination + Comfort + Activities Act-Check');
-            PushToListWithoutDuplicate(searchVerifiedLocations, i); continue;
+            dateValidationAndSearchResult(); continue;
           }
         }
         else if ((searchParameters[0] != "none") && (searchParameters[2].length != 0)) {
           if ((arrayItemComparer(searchParameters[2], data.resorts[i].activities) == true) && (searchParameters[2].length !== 0) && (searchParameters[0] == data.resorts[i].destination) && (data.resorts[i].price > searchPriceRange[0]) && (data.resorts[i].price < searchPriceRange[1])) {
             console.log('Destination + Activities');
-            PushToListWithoutDuplicate(searchVerifiedLocations, i); continue;
+            dateValidationAndSearchResult(); continue;
           }
         }
         else if ((searchParameters[1] != "none") && (searchParameters[0] != "none")) {
@@ -144,6 +158,7 @@ $("document").ready(function(){
         searchedLocationsDisplay += "<p> Name: "+data.resorts[searchVerifiedLocations[y]].name+"</p>";
         searchedLocationsDisplay += "<p> Destination: "+data.resorts[searchVerifiedLocations[y]].destination+"</p>";
         searchedLocationsDisplay += "<p> Price: "+data.resorts[searchVerifiedLocations[y]].price+"</p>";
+        searchedLocationsDisplay += "<p> Start Date: "+data.resorts[searchVerifiedLocations[y]].startDate+" End Date: "+data.resorts[searchVerifiedLocations[y]].endDate+"</p>";
         searchedLocationsDisplay += "<p> Comfort Level: "+data.resorts[searchVerifiedLocations[y]].comfortLevel+"</p>";
         searchedLocationsDisplay += "<p> Activities Offered: "+data.resorts[searchVerifiedLocations[y]].activities+"</p>";
         searchedLocationsDisplay += "</div>";
