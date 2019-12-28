8d208b1aa514d70077a89de1c34e1cf9d4bab884
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2019-12-28 20:33:18 +0530

    Resort Page Updates

--- script/resort-receive.js
+++ script/resort-receive.js
@@ -4,10 +4,26 @@ var queries = queryString.split("&");
 var newVar= [];
 newVar = queries[0].split("=");
 userPosition=newVar[1];
+
+function thisGalleryImage(image){
+  var newImage = image.src;
+  $("#BigBannerImage").attr("src", newImage);
+}
+
 $(document).ready( function() {
-  console.log(userPosition);
+  var resortGalleryImages = "";
   $.getJSON('/json/locations.json', function(data){
     window.document.title = data.resorts[userPosition].name + " | Resorts";
-    document.getElementById("headerTitle").innerHTML = data.resorts[userPosition].name;
+    $("#headerTitle").text(data.resorts[userPosition].name);
+    $("#resort_TitleHeading").text(data.resorts[userPosition].name);
+    $("#BigBannerImage").attr("src", data.resorts[userPosition].picture);
+    resortGalleryImages += "<img src='"+data.resorts[userPosition].picture+"' class='smallBannerClick' onclick='thisGalleryImage(this)'>";
+    for (var i in data.resorts[userPosition].gallery) {
+      resortGalleryImages += "<img src='"+data.resorts[userPosition].gallery[i]+"' class='smallBannerClick' onclick='thisGalleryImage(this)'>";
+    }
+    $("#SmallGalleryImages").html(resortGalleryImages);
+    $(".smallBannerClick").on("click", thisGalleryImage(this));
+    console.log(resortGalleryImages);
+    $("#resort_ShortDescription").text(data.resorts[userPosition].short_description);
   });
 });
