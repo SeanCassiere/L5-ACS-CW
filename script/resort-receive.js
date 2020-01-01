90ca4480ceb5e542d4d41eddeb5e891f392ea197
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-01-02 00:07:42 +0530

    Commenting

--- script/resort-receive.js
+++ script/resort-receive.js
@@ -1,3 +1,4 @@
+// Read Resort Position for locations.json file from URL Bar
 var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
 var queries = queryString.split("&");
@@ -5,6 +6,7 @@ var newVar= [];
 newVar = queries[0].split("=");
 userPosition=newVar[1];
 
+// Click to update Gallery Image
 function thisGalleryImage(image){
   var newImage = image.src;
   $("#BigBannerImage").attr("src", newImage);
@@ -14,33 +16,36 @@ $(document).ready( function() {
   var resortGalleryImages = "";
   var activitiesOnOffer = "";
   $.getJSON('/json/locations.json', function(data){
-    window.document.title = data.resorts[userPosition].name + " | Resorts";
-    $("#headerTitle").text(data.resorts[userPosition].name);
+    window.document.title = data.resorts[userPosition].name + " | Resorts";// Write Resort name to page/tab Title
+    $("#headerTitle").text(data.resorts[userPosition].name); // Write Resort name to Title
     
-    $("#resort_TitleHeading").text(data.resorts[userPosition].name);
+    $("#resort_TitleHeading").text(data.resorts[userPosition].name); // Write Resort Name
     
-    $("#BigBannerImage").attr("src", data.resorts[userPosition].picture);
+    $("#BigBannerImage").attr("src", data.resorts[userPosition].picture); // Write Big Gallery Banner Image
+
+    // Read Gallery Images
     resortGalleryImages += "<img src='"+data.resorts[userPosition].picture+"' class='smallBannerClick' onclick='thisGalleryImage(this)'>";
     for (var i in data.resorts[userPosition].gallery) {
       resortGalleryImages += "<img src='"+data.resorts[userPosition].gallery[i]+"' class='smallBannerClick' onclick='thisGalleryImage(this)'>";
     }
-    $("#SmallGalleryImages").html(resortGalleryImages);
-    $(".smallBannerClick").on("click", thisGalleryImage(this));
+    $("#SmallGalleryImages").html(resortGalleryImages); // Write Gallery Images
+    $(".smallBannerClick").on("click", thisGalleryImage(this)); // Gallery Image 'CLICK' event listener
 
-    $("#resort_ShortDescription").text(data.resorts[userPosition].short_description);
+    $("#resort_ShortDescription").text(data.resorts[userPosition].short_description); // Write to Brief Description
 
+    // Read Activities
     for (var u in data.resorts[userPosition].activities) {
       activitiesOnOffer += data.resorts[userPosition].activities[u]+", ";
     }
     activitiesOnOffer = activitiesOnOffer.substring(0, activitiesOnOffer.length - 2);
-    $("#resort_ActivitiesOffered").text(activitiesOnOffer);
+    $("#resort_ActivitiesOffered").text(activitiesOnOffer); // Write to Activities
     
-    $("#resort_Destination").text("Location: "+data.resorts[userPosition].location);
+    $("#resort_Destination").text("Location: "+data.resorts[userPosition].location); // Write to Location
 
-    $("#resort_Price").text("Current Rate: $"+data.resorts[userPosition].price+"*");
+    $("#resort_Price").text("Current Rate: $"+data.resorts[userPosition].price+"*"); //Write to Current Rate
 
-    $("#resort_Tabs").tabs( {collapsible: true} );
-    $("#resort_LongDescription").html(data.resorts[userPosition].long_description);
-    $("#resort_GoogleMaps").attr("src", data.resorts[userPosition].google_map);
+    $("#resort_Tabs").tabs( {collapsible: true} ); // Make Tabs jQueryUI
+    $("#resort_LongDescription").html(data.resorts[userPosition].long_description); // Write to Tab
+    $("#resort_GoogleMaps").attr("src", data.resorts[userPosition].google_map); // Write to Tab
   });
 });
